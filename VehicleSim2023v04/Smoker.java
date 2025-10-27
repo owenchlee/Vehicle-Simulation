b811e060027b305c26290632bbe4feee7bfc6ead
owenchlee <owenlees888@gmail.com>
2025-10-27 18:44:41 -0400

    all sounds working

--- VehicleSim2023v04/Smoker.java
+++ VehicleSim2023v04/Smoker.java
@@ -8,18 +8,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Smoker extends Pedestrian
 {
+    private GreenfootSound smoke;
     
     public Smoker (int direction){
         super(direction);
         for (GreenfootImage frame : smokeMan.getImages()) {
             frame.scale(80, 80); // scale every frame in the GIF
         }
+        smoke = new GreenfootSound ("smoking.mp3");
+        smoke.setVolume(30);
     }
     
     private GifImage smokeMan = new GifImage("smoker.gif");
     public void act()
     {
         setImage(smokeMan.getCurrentImage());
+        smoke.play();
         super.act();
     }
 }
